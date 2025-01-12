605a6c23a663bec89a4060abd7e683ceae0c46b9
Gino Zhang <ginobefun@163.com>
2025-01-12 21:00:45 +0800

    upgrade dependency version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+  },
+  experimental: {
+    typedRoutes: true,
+  },
+}
 
 module.exports = withContentlayer(nextConfig)
